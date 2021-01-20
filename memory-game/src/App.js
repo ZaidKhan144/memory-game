dd754c4a00f2f67b3275c64ad1811e1ef5099ca1
ZaidKhan144 <zaidirfan.945@gmail.com>
2021-01-20 13:22:30 -0600

    intro text

---
+++ memory-game/src/App.js
@@ -0,0 +1,11 @@
+import Intro from './component/Intro'
+
+function App() {
+  return (
+    <div>
+      <Intro />
+    </div>
+  );
+}
+
+export default App;
---
+++ memory-game/src/component/Intro.js
@@ -0,0 +1,16 @@
+
+const Intro = () => {
+    return(
+        <div>
+            <h1>How it works</h1>
+            <p>Welcome to the Gifs Memory Game</p>
+            <p>This memory game will present you with random gifs on screen.</p> 
+            <p>To gain points, select GIFs that are different from the previous one you selected. 
+            If you choose the same gif more than once, you will lose.</p>
+            <button>Ok, I got it</button>
+        </div>
+        
+    )
+}
+
+export default Intro
