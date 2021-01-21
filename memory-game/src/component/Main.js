8a0b31303b495764c92d825a85995870ee89b830
ZaidKhan144 <zaidirfan.945@gmail.com>
2021-01-20 20:58:36 -0600

    gifs rendering on screen

--- memory-game/src/component/Main.js
+++ memory-game/src/component/Main.js
@@ -1,8 +1,25 @@
+import React, {useState, useEffect} from 'react'
 
 const Main = () => {
+
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetch('https://api.giphy.com/v1/gifs/trending?api_key=aIINKf0Pxb8MDhC2QPzTLbgvUXN6Uz7l')
+        .then(response => response.json())
+        .then(responseData => setData(responseData.data))
+        .catch(error => {
+            console.log('Error fetching and parsing data', error)
+          })
+    },[])
+
+    let gif = data.map(gif => 
+            <img src={gif.images.fixed_height.url} key={gif.id} alt=""/>
+        )
+    
     return(
         <div className="main hide">
-            <h4>Over here my GIFs will come</h4>
+            {gif}
         </div>
     )
 }
