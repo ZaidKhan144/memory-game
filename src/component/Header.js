8e1abb5f45fd85afbcd1489cc55f7bab1c7aac84
ZaidKhan144 <zaidirfan.945@gmail.com>
2021-02-08 19:59:09 -0600

    git icon

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,10 +1,30 @@
+import styled from "styled-components";
+import { Github } from "@styled-icons/feather";
+
 
 const Header = () => {
+
+    const GitLogo = styled(Github)`
+            margin-top: 5px;
+            width: 30px;
+            height: 30px;
+            transition: all .5s ease;
+            fill: transparent;
+
+            &:hover{
+                fill: #fff;
+                transform: translateY(5px);
+            }
+    `
+
     return(
-        <div>
+        <div className="header-daddy">
             <div className="header hide">
-                <h1>Memory&nbsp; GIFs&nbsp; Game</h1>
-            </div>
+                <div>
+                    <h1>Memory&nbsp; GIFs&nbsp; Game</h1>
+                    <a href="https://github.com/ZaidKhan144" target="_blank" rel="noreferrer"><GitLogo></GitLogo></a>
+                </div>
+             </div>
         </div>
     )
 }
